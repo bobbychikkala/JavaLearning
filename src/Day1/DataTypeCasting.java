0b0029fda555943cc757a7095017ab2ac4818e72
bobbychikkala <chikkalabobbyk2@gmail.com>
2025-05-08 20:57:30 +0530

    first commit

---
+++ src/Day1/DataTypeCasting.java
@@ -0,0 +1,42 @@
+package Day1;
+
+public class DataTypeCasting {
+	Object myin = 77;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a= (int)45.45 ;
+		//System.out.println(a);
+		//System.out.print(a);
+		
+		
+		short s2 = 136;
+		
+		byte b2 = (byte)s2;
+		
+		System.out.println(b2);
+		
+		
+		float  f3 = 333.999f;
+		int i3 = (int)Math.ceil(f3);
+		System.out.println(i3);
+		
+		char c ='4';
+		int i5 = c;
+		System.out.println(i5);
+
+		int num = 1077;  
+		Integer objNum = Integer.valueOf(num);  // Convert int to Integer (wrapper)
+		System.out.println(objNum);
+		int i10 = 112;
+		//System.out.println(type(i10));
+		
+		Object myin = 77;
+		
+	}
+
+
+	
+
+}
---
+++ src/Day1/MyFirst.java
@@ -0,0 +1,18 @@
+package Day1;
+
+public class MyFirst {
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		@SuppressWarnings("unused")
+		int a = 10;
+		System.out.println(10);
+		
+
+	
+	}
+
+}
