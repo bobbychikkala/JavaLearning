b732c5d569933a4ece6864699c18c0ca56b2a576
bobbychikkala <chikkalabobbyk2@gmail.com>
2025-05-08 22:53:04 +0530

    changes

--- src/Day2/SwitchCase.java
+++ src/Day2/SwitchCase.java
@@ -6,9 +6,11 @@ public class SwitchCase {
 	{
 		String day = weekDay(7);
 		System.out.println(day);
+		System.out.println(monthName(1));
 	}
 	
 	static String weekDay(int dayNumber) 
+
 	{
 		
 		switch(dayNumber) 
@@ -36,4 +38,44 @@ public class SwitchCase {
 		
 		
 	}
+
+
+static String monthName(int MonthNumber) 
+
+{
+	
+	switch(MonthNumber) 
+	{
+	case 1 :
+		return "January";
+	case 2 :
+		return "Febuary";
+	case 3 :
+		return "March";
+	case 4 :	
+		return "April";
+	case 5 :
+		return "May";
+	case 6 :
+		return "June";
+	case 7 :
+		return "July";
+	case 8 :	
+		return "August";
+	case 9 :
+		return "September";
+	case 10 :
+		return "October";
+	case 11 :
+		return "November";
+	case 12 :
+		return "December";
+	default:
+		System.out.println("Invalid number");
+		return "Invalid number" ;
+	
+	}
+	
+	
+}
 }
