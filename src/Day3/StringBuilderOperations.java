f849c30d217e9daeb541511da4c6d628157e14d1
bobbychikkala <chikkalabobbyk2@gmail.com>
2025-05-10 19:55:37 +0530

    String Builder

---
+++ src/Day3/StringBuilderOperations.java
@@ -0,0 +1,25 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class StringBuilderOperations {
+    private static Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String string = "Welcome to the JAVA Programming";
+		
+		
+		StringBuilder sb = new StringBuilder(string);
+		System.out.println(sb);
+		System.out.println(sb.reverse());
+		System.out.println(sb.length());
+		
+		String x = scanner.next();
+		
+		int number = Integer.parseInt(x);
+		
+		
+	}
+
+}
