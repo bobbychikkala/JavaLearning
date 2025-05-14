352a6bae0e65cf6c7140120547b3752c7276336c
bobbychikkala <chikkalabobbyk2@gmail.com>
2025-05-14 15:36:36 +0530

    reverse numbers

---
+++ src/ExternalProblems/Day5.java
@@ -0,0 +1,29 @@
+package ExternalProblems;
+
+public class Day5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(reverseNumber(123));
+	}
+	
+	private static int reverseNumber(int input) {	
+	
+	int result = 0;
+	int module;
+	
+	while(input!=0)
+	{
+		module =input%10 ;
+		input /=10;
+		result *=10 ;
+		result +=module ;
+		
+		
+	}
+	return result ;
+	}
+	
+
+}
