4b84a8679664b57a2f7c524e93fb04a744bf3d83
bobbychikkala <chikkalabobbyk2@gmail.com>
2025-05-16 21:42:23 +0530

    Reverse words

--- src/Day3/StringOperations.java
+++ src/Day3/StringOperations.java
@@ -1,5 +1,6 @@
 package Day3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -12,39 +13,18 @@ public class StringOperations {
 		
 		String string = "Hello Wolrd !";
 		 
-		
-		
-		// printing the string length
-		
-		System.out.println(string.length());
-		
-		// string iteration
-		String reverse="";
-		for(int i =string.length()-1; i>=0;i--){
-			//System.out.println(string.charAt(i));
-			reverse+=string.charAt(i);
-		}
-		
-		//System.out.println(reverse);
-		
 		System.out.println(isPolidram("Madam"));
 		
-		System.out.println(reverseWords("Hello Java Program"));
-
+		System.out.println(reversWords1("Hello Java Program"));
 	}
 	
-	private static boolean isPolidram(String input) {
-		
-		String reverse="";
-		input =input.toLowerCase();
-		for (int i=input.length()-1;i>=0;i--) {
-			
-			reverse += input.charAt(i);
-			
+	private static boolean isPolidram(String input) {		
+		String reverse="";	
+		for (int i=input.length()-1;i>=0;i--) {			
+			reverse += input.charAt(i);			
 		}
-		
-		return input.equals(reverse);
-	
+		System.out.println(reverse);
+		return input.equalsIgnoreCase(reverse);	
 	}
 	
 
@@ -62,5 +42,27 @@ public class StringOperations {
 		
 		
 	}
+	
+	private static String reversWords1(String input) {
+		
+		ArrayList <String> words = new ArrayList();
+		String temp="";
+		input=input+"  " ;	
+		for (int i =0; i< input.length()-1 ; i++) {
+			
+			if (input.charAt(i)==' ') {			
+				words.add(temp);
+				temp="";
+				}
+			else temp+=input.charAt(i);			
+		}
+		String result="";
+		for(var a:words) result=a+" "+result ;
+			
+			
+		return result;
+		
+	}
+	
 	
 }
