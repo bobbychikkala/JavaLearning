2729ce87729258a2f7182adf92193df1c086043d
bobbychikkala <chikkalabobbyk2@gmail.com>
2025-05-22 20:43:22 +0530

    Trainagle Pattern

--- src/externalProblems/Pattern.java
+++ src/externalProblems/Pattern.java
@@ -6,10 +6,10 @@ public class Pattern {
 
 	public static void main(String []args) {
 		reverseTrai(6);
-		//System.out.println();
+		
 		trianglePattern(6);
-		//revTraig(5);
-		//triag(5);
+		
+		
 	}
 	
 	public static void trianglePattern(int n ) {
@@ -47,30 +47,8 @@ public class Pattern {
 		}
 	}
 	
-	static void triag(int n) {
-		
-		for (int i=0;i<n;i++) {
-			
-			for (int j=0;j<n-i-1;j++) {
-				System.out.print(" ");
-			}
-			for (int k=0;k<2*i+1;k++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
-		
-	}
 	
-	static void revTraig(int n) {
 		
-		for(int i =0 ; i<n;i++) {
-			for (int j=0; j<(n-i)*2-1;j++) System.out.print("*");
-				System.out.println();
-			for (int k=0;k<i+1;k++)	System.out.print(" ");
-		}
 		
-		}
 	}
 
